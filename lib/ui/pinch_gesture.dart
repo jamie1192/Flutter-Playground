d07b53644875b8906bcfd0d637d191fc7e5d7642
jamie1192 <jastley92@gmail.com>
2019-02-13 15:15:32 +1100

    Pinch now resizes image and text

--- lib/ui/pinch_gesture.dart
+++ lib/ui/pinch_gesture.dart
@@ -25,43 +25,75 @@ class _PinchImage extends StatefulWidget {
 
 class _PinchImageState extends State<_PinchImage> {
 
-  double size = 200.0;
-  double temp = 0.0;
+  double constSize = 250.0;
+  double dynamicSize = 250.0;
+
+  double constFont = 20.0;
+  double dynamicFontSize = 20.0;
+
+  double constTop = 80.0;
+  double dynamicTop = 80.0;
+  double constLeft = 90.0;
+  double dynamicLeft = 90.0;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
           onScaleUpdate: (newScale) {
 
-              Fimber.i('scale: ${newScale.scale}');
-              double tempScale = newScale.scale * 10;
+            Fimber.i('scale: ${newScale.scale}');
+            double tempScale = newScale.scale;
 
-              double tempSize = size + tempScale;
-              Fimber.i('temp: $tempScale, tempSize: $tempSize');
-//              if (newScale.scale < 1.5 && newScale.scale >= 0.25) {
-              if (tempSize >= 50.0 && newScale.scale <= 350.0) {
-                setState(() {
-//                  double val = size * newScale.scale;
-                  Fimber.i('size: $tempScale');
-                  size = tempSize;
-                });
+            double tempSize = constSize * tempScale;
+            double tempFont = constFont * tempScale;
 
-              }
-//              if (newScale.scale >= 30 && newScale.scale <= 500) {
+            double tempLeft = constLeft * tempScale;
+            double tempTop = constTop * tempScale;
+            Fimber.i('temp: $tempScale, tempSize: $tempSize');
 
-//            }
+            if (tempSize >= 50.0 && tempSize <= 400.0) {
+              setState(() {
+                Fimber.i('size: $tempScale');
+                dynamicSize = tempSize;
+                dynamicFontSize = tempFont;
+                dynamicTop = tempTop;
+                dynamicLeft = tempLeft;
+              });
+            }
+
+          },
+          onScaleEnd: (scaleEnd) {
+            Fimber.i('dynamicSize: $dynamicSize');
           },
           child: Container(
             height: double.infinity,
             width: double.infinity,
+            color: Colors.white,
             child: Center(
-              child:  Image(
-                image: AssetImage('assets/images/icon_cloud.png'),
-                width: size,
-                height: size,
+              child: Stack(
+                alignment: FractionalOffset.center,
+                children: <Widget>[
+                  Image(
+                    image: AssetImage('assets/images/icon_cloud.png'),
+                    width: dynamicSize,
+                    height: dynamicSize,
+                  ),
+                  Positioned(
+                    top: dynamicTop,
+                    left: dynamicLeft,
+                    child: Text(
+                        'Cloud Text',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: dynamicFontSize,
+                        fontFamily: 'Children-One'
+                      ),
+                    )
+                  )
+                ],
+              )
+            ),
           ),
-        ),
-      ),
     );
   }
 
